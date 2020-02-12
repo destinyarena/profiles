df0a35c31511bcc115579e88c065d2831837a1f0
Arturo Guerra <ar2roguerra@gmail.com>
2020-02-12 20:55:01 +0000

    Added ban command

--- cmd/profiles/ban.go
+++ cmd/profiles/ban.go
@@ -1,15 +1,15 @@
 package main
 
-//import (
-//    "context"
-//    pb "github.com/arturoguerra/destinyarena-accounts/proto"
-//)
+import (
+    "context"
+    pb "github.com/arturoguerra/destinyarena-accounts/proto"
+)
 
-//func (p *ProfilesServer) Ban(ctx context.Context, in *pb.IdRequest) (*pb.Empty, error) {
-//    if err := p.DB.Ban(in.GetId()); err != nil {
-//        log.Error(err)
-//        return nil, err
-//    }
-//
-//    return &pb.Empty{}, nil
-//}
+func (p *ProfilesServer) Ban(ctx context.Context, in *pb.IdRequest) (*pb.Empty, error) {
+    if err := p.DB.Ban(in.GetId()); err != nil {
+        log.Error(err)
+        return nil, err
+    }
+
+    return &pb.Empty{}, nil
+}
--- pkg/database/ban.go
+++ pkg/database/ban.go
@@ -1,1 +1,22 @@
 package database
+
+/*
+Marks user as banned
+*/
+
+func (f *DBClient) Ban(uid string) error {
+    err, user := f.GetUser(uid)
+    if err != nil {
+        return err
+    }
+
+    db, err := f.Connect()
+    if err != nil {
+        return err
+    }
+    defer db.Close()
+
+    user.Banned = true
+    db.Save(user)
+    return nil
+}
